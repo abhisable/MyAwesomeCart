0657f77fa1aba113aae96ebdf5794e56d16a8e88
abhisable <abhibtl2018@gmail.com>
2019-07-28 00:59:45 -0700

    first commit

---
+++ shop/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+# Create your views here.
+def index(request):
+    return render(request,'shop/index.html')
+def about(request):
+    return HttpResponse("you are on about page")
+def contact(request):
+    return  HttpResponse("you are on contact page")
+def tracker(request):
+    return HttpResponse("you are on tracker page")
+def search(request):
+    return HttpResponse("you are on search page")
+def productView(request):
+    return HttpResponse("you are on prodView page")
+def checkout(request):
+    return HttpResponse("you are on checkout page")
+def product(request):
+    context={'product':Product.objects.all()}
+    return render(request,'shop/product.html',context)
+
