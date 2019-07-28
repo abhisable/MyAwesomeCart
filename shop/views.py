8883c3577846dd1d674981c6b9d09f628711e2b7
abhisable <abhibtl2018@gmail.com>
2019-07-28 05:48:51 -0700

    second commit

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
+from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'shop/index.html')
 def about(request):
-    return HttpResponse("you are on about page")
+    return render(request,'shop/about.html')
 def contact(request):
     return  HttpResponse("you are on contact page")
 def tracker(request):
