4c0487cffab1e7d17f2cb89b4688a0907b0848b3
abhisable <abhibtl2018@gmail.com>
2019-08-21 18:01:03 +0530

    Tracker for order by Order Id

--- shop/views.py
+++ shop/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
-from .models import Product,Contact,Orders
+from .models import Product,Contact,Orders,OrderUpdate
 from math import ceil
-
+import json
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    #products = Product.objects.all()
-    #print(products)
-    #n = len(products)
-    #nSlides = n//4 + ceil((n/4)-(n//4))
-    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
-    #allProds=[[products,range(1,nSlides),nSlides],[products,range(1,nSlides),nSlides]]
+    
     allProds=[]
     catProds=Product.objects.values('category','product_id')
     cats={item['category'] for item in catProds}
@@ -39,7 +34,25 @@ def contact(request):
     return render(request,'shop/contact.html')
 
 def tracker(request):
+    if request.method == "POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order) > 0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps(updates, default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
+
     return render(request, 'shop/tracker.html')
+
 
 def search(request):
     return render(request, 'shop/search.html')
@@ -61,7 +74,10 @@ def checkout(request):
         phone=request.POST.get('phone','')
         
         order=Orders(items_json=items_json,name=name,email=email,address=address,city=city,state=state,zip_code=zip_code,phone=phone)
+        
         order.save()
+        update=OrderUpdate(order_id=order.order_id,update_desc="The order has been placed")
+        update.save()
         thank=True
         id=order.order_id
         return render(request,'shop/checkout.html',{'thank':thank,'id':id})
