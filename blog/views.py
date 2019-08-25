357ac930aad47bdbbc40b56e54d71f2e99ab420b
abhisable <abhibtl2018@gmail.com>
2019-08-25 12:28:42 +0530

    created model for blogpost

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Blogpost
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
+   
     return render(request, "blog/index.html")
 def blogpost(request):
+
     return render(request,"blog/blogpost.html")
